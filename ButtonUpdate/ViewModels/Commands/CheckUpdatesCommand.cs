a734f97957e8917c6374a2045c8ee598a2361022
Olezha228 <87082100+Olezha228@users.noreply.github.com>
2021-09-29 09:51:41 +0500

    Add files via upload

---
+++ ButtonUpdate/ViewModels/Commands/CheckUpdatesCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ButtonUpdate.ViewModels.Commands
+{
+    public class CheckUpdatesCommand : ICommand
+    {
+        public ViewModelBase ViewModel { get; set; }
+
+        public CheckUpdatesCommand(ViewModelBase viewModelBase)
+        {
+            this.ViewModel = viewModelBase;
+        }
+
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModel.CheckUpdatesAfterClick();
+        }
+    }
+}
---
+++ ButtonUpdate/ViewModels/ViewModelBase.cs
@@ -0,0 +1,40 @@
+﻿using ButtonUpdate.Model;
+using ButtonUpdate.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+
+namespace ButtonUpdate.ViewModels
+{
+    public class ViewModelBase
+    {
+        public CheckUpdatesCommand SimpleCommand { get; set; }
+
+        public ViewModelBase()
+        {
+            SimpleCommand = new CheckUpdatesCommand(this);
+        }
+
+        public void CheckUpdatesAfterClick()
+        {
+            if (Updates.AreFound())
+            {
+
+            }
+            else
+            {
+                Internet internet = new Internet();
+                if (internet.IsConnected())
+                    MessageBox.Show("Connected!");
+                else
+                    MessageBox.Show("Not Connected!");
+
+            }
+        }
+
+
+
+    }
+}
